c754a508f10f04dd7aab13ca6beade2fd0f75d9c
Tadele Melak Gashayie <59862645+Tadie-ops@users.noreply.github.com>
2024-03-04 19:14:41 +1100

    Update data_retrieval_curation.R

--- data_retrieval_curation.R
+++ data_retrieval_curation.R
@@ -9,8 +9,34 @@ entrez_db_links("gds")
 #### searching term 
 term<-"(gse[ETYP]) AND (high-throughput sequencing[PTYP]) AND
 (Serum[Title/Abstract/SRC/SFIL] OR plasma[Title/Abstract/SRC/SFIL] OR circulat* [Title/Abstract/SRC/SFIL] OR blood[Title/Abstract/SRC/SFIL]) AND (human[Organism]) AND (2000/01/01[PDAT] : 2023/07/19[PDAT])"
+
+
+
 api<-"01b5cbc1ccc16bfbd940e9a02ef8ebaf040a"
 search <- entrez_search(db="gds", term=term, use_history=TRUE)
+
+
+iteration<-ceiling(count/500)
+c<-0
+summary<-list()
+for(i in 1:iteration){
+  
+  summary[[i]]<-entrez_summary(db="gds", web_history=search$web_history, api_key =api, retstart=c, retmax=500)
+  c<-c+500
+
+  
+features<-c("uid", "accession", "gds", "title", "summary", "gpl", "gse", "taxon","entrytype","gdstype","ptechtype", "valtype",
+            "ssinfo","subsetinfo","pdat","suppfile",     
+            "samples", "relations", "extrelations", "n_samples", "seriestitle",  "platformtitle", "platformtaxa","samplestaxa"  
+            ,"pubmedids","projects","ftplink","geo2r")  
+
+####. The number of features that can be extracted
+
+features
+
+
+
+  
 
 
 
