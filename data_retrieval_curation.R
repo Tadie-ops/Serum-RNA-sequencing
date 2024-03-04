88bbb8a081427e1328edbb9ca411a47515e3382b
Tadele Melak Gashayie <59862645+Tadie-ops@users.noreply.github.com>
2024-03-04 19:09:33 +1100

    Create data_retrieval_curation.R

---
+++ data_retrieval_curation.R
@@ -0,0 +1,13 @@
+#### list of entrez databases
+##entrez_dbs()
+## general description of gds database which is GEO database
+entrez_db_summary("gds")
+## Searchable fields for gds database 
+entrez_db_searchable("gds")
+### To return the linked databases with gds database
+entrez_db_links("gds")
+#### searching term 
+term<-"(gse[ETYP]) AND (high-throughput sequencing[PTYP]) AND
+(Serum[Title/Abstract/SRC/SFIL] OR plasma[Title/Abstract/SRC/SFIL] OR circulat* [Title/Abstract/SRC/SFIL] OR blood[Title/Abstract/SRC/SFIL]) AND (human[Organism]) AND (2000/01/01[PDAT] : 2023/07/19[PDAT])"
+api<-"01b5cbc1ccc16bfbd940e9a02ef8ebaf040a"
+search <- entrez_search(db="gds", term=term, use_history=TRUE)
