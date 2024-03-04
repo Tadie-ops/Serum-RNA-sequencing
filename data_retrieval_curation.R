633c0f575c0294ad72f9d2e052b0126a5c0d8b84
Tadele Melak Gashayie <59862645+Tadie-ops@users.noreply.github.com>
2024-03-04 19:16:11 +1100

    Update data_retrieval_curation.R

--- data_retrieval_curation.R
+++ data_retrieval_curation.R
@@ -24,15 +24,39 @@ for(i in 1:iteration){
   summary[[i]]<-entrez_summary(db="gds", web_history=search$web_history, api_key =api, retstart=c, retmax=500)
   c<-c+500
 
-  
+  ####. The number of features that can be extracted
 features<-c("uid", "accession", "gds", "title", "summary", "gpl", "gse", "taxon","entrytype","gdstype","ptechtype", "valtype",
             "ssinfo","subsetinfo","pdat","suppfile",     
             "samples", "relations", "extrelations", "n_samples", "seriestitle",  "platformtitle", "platformtaxa","samplestaxa"  
             ,"pubmedids","projects","ftplink","geo2r")  
 
-####. The number of features that can be extracted
 
-features
+
+df.original<-lapply(summary, function(x) extract_from_esummary(x, features))
+df.original<-do.call(cbind, df.original)
+##head(df[,1:5])
+df.original<-t(df.original)
+nrow<-nrow(df.original)
+
+##df$samples[1:2]
+df.study<-data.frame(seq(1:nrow))
+for(i in 1:28)
+{
+  temp<-unlist(df.original[,i])
+  
+  if(!is.null(temp)&&length(temp)==nrow)
+  { 
+  df.study<-cbind(df.study,temp)
+  y<-ncol(df.study)
+  colnames(df.study)[y]<-colnames(df.original)[i]
+  } 
+ 
+}
+  
+##### 
+df.study<-df.study[,-1]
+write.csv(df.study, file="df.study.csv")
+write.csv(df.original, file="df.original.csv")
 
 
 
