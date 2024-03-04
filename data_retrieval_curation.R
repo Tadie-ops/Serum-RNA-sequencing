683b95eedc68639c62a97f7e1c30caaa9e5c3509
Tadele Melak Gashayie <59862645+Tadie-ops@users.noreply.github.com>
2024-03-04 19:12:32 +1100

    Update data_retrieval_curation.R

--- data_retrieval_curation.R
+++ data_retrieval_curation.R
@@ -11,3 +11,6 @@ term<-"(gse[ETYP]) AND (high-throughput sequencing[PTYP]) AND
 (Serum[Title/Abstract/SRC/SFIL] OR plasma[Title/Abstract/SRC/SFIL] OR circulat* [Title/Abstract/SRC/SFIL] OR blood[Title/Abstract/SRC/SFIL]) AND (human[Organism]) AND (2000/01/01[PDAT] : 2023/07/19[PDAT])"
 api<-"01b5cbc1ccc16bfbd940e9a02ef8ebaf040a"
 search <- entrez_search(db="gds", term=term, use_history=TRUE)
+
+
+
